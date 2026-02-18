102435e875ff614b9ff0ad1207d7496655eb04df
dianethv <dianethvaldez@gmail.com>
2026-02-18 16:41:54 -0600

    fixed app and main (mounting issues)

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
-/*
-//orginally created main.jsx from host
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
-*/
-/*update main.jsx */
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { AppProvider } from './context/AppContext'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
