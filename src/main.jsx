57cfbf3b4e9fc40c77a01a51a744f87f8a33d5bf
bdang767 <ldang9@cougarnet.uh.edu>
2026-02-18 14:22:25 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+/*
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -7,4 +8,22 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
+)
+*/
+/*update main.jsx */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { AppProvider } from './context/AppContext'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
